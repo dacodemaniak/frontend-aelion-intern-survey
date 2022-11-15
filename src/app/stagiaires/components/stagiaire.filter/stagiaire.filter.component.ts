589bd94cab4fdc7594285df54eb50eec994ac924
DaCodeManiaK <jla.webprojet@gmail.com>
2022-11-15 15:05:36 +0100

    Add Input decorator

--- src/app/stagiaires/components/stagiaire.filter/stagiaire.filter.component.ts
+++ src/app/stagiaires/components/stagiaire.filter/stagiaire.filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-stagiaire-filter',
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StagiaireFilterComponent implements OnInit {
 
+  @Input() public filterDate: Date | null = null;
+
+  private buttonMap: Map<string, boolean> = new Map<string, boolean>();
+
   constructor() { }
 
   ngOnInit(): void {
+    this.buttonMap.set('btnAll', true);
+    this.buttonMap.set('btnLT1950', false);
+    this.buttonMap.set('btnGT1950', false);
+
+    if (this.filterDate === null) {
+      this.changeButtonState('btnAll');
+    } else {
+      // SI le jour est 31 donc, je veux les personnes néees après 1950
+      // SINON je veux les personnées néees avant 1950
+      if (this.filterDate.getDate() === 31) {
+        this.changeButtonState('btnGT1950');
+      } else {
+        this.changeButtonState('btnLT1950');
+      }
+    }
+  }
+
+  public getButtonState(key: string): boolean {
+    return this.buttonMap.get(key)!;
+  }
+
+  public changeButtonState(button: string): void {
+    // Have to change the value of key to true, others to false
+    this.buttonMap.forEach((value: boolean, key: string) => {
+      if (key === button) {
+        this.buttonMap.set(key, true);
+      } else {
+        this.buttonMap.set(key, false);
+      }
+    });
   }
 
 }
