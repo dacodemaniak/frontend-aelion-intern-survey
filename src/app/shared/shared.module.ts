c08f44a41882fadf25f00dbc596d9b604254ff37
DaCodeManiaK <jla.webprojet@gmail.com>
2022-11-28 16:27:57 +0100

    Configure MAT_MOMENT adapters and locales

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,20 @@ import { CommonModule } from '@angular/common';
 import { MaterialUiModule } from '../material-ui/material-ui.module';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { 
+  MatMomentDateModule,
+  MomentDateAdapter,
+  MAT_MOMENT_DATE_ADAPTER_OPTIONS,
+  MAT_MOMENT_DATE_FORMATS
+} from '@angular/material-moment-adapter';
+import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE,
+  
+} from '@angular/material/core';
 
+import 'moment/locale/fr';
 
 
 @NgModule({
@@ -14,7 +27,25 @@ import { ReactiveFormsModule } from '@angular/forms';
   exports: [
     MaterialUiModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatMomentDateModule
+  ],
+  providers: [
+    {
+      provide: MAT_DATE_LOCALE, useValue: 'fr-FR'
+    },
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [
+        MAT_DATE_LOCALE,
+        MAT_MOMENT_DATE_ADAPTER_OPTIONS
+      ]
+    },
+    {
+      provide: MAT_DATE_FORMATS,
+      useValue: MAT_MOMENT_DATE_FORMATS
+    }
   ]
 })
 export class SharedModule { }
