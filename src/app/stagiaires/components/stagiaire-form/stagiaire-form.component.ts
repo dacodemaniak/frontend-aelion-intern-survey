673a86460b781bb7b4d3ffbd98649d93e1875a40
DaCodeManiaK <jla.webprojet@gmail.com>
2022-11-28 15:10:38 +0100

    Add required validator to phoneNumber

--- src/app/stagiaires/components/stagiaire-form/stagiaire-form.component.ts
+++ src/app/stagiaires/components/stagiaire-form/stagiaire-form.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Stagiaire } from 'src/app/core/models/stagiaire';
 import { StagiaireService } from 'src/app/core/services/stagiaire.service';
+import { StagiaireDto } from '../../dto/stagiaire-dto';
+import { FormBuilderService } from '../../services/form-builder.service';
 
 @Component({
   selector: 'app-stagiaire-form',
@@ -9,39 +11,22 @@ import { StagiaireService } from 'src/app/core/services/stagiaire.service';
   styleUrls: ['./stagiaire-form.component.scss']
 })
 export class StagiaireFormComponent implements OnInit {
-
   // stagiaire: Stagiaire = new Stagiaire()
 
-  stagiaireForm: FormGroup = new FormGroup({
-      lastname: new FormControl('', 
-          Validators.required),
-      firstname: new FormControl('', Validators.required),
-      email: new FormControl('', [
-          Validators.required, 
-          Validators.email]),
-      phoneNumber: new FormControl('', 
-          Validators.pattern("^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$")),
-      birthdate: new FormControl(null),
-  })
+  stagiaireForm!: FormGroup;
 
-  constructor(private stagiaireService: StagiaireService) { }
+  constructor(
+    private stagiaireService: StagiaireService,
+    private formBuilderService: FormBuilderService) { }
 
   ngOnInit(): void {
+    this.stagiaireForm = this.formBuilderService.build().getForm();
   }
 
   onSubmit() {
-    // TODO: Use EventEmitter with form value
-    console.log("Read from form:", this.stagiaireForm.value)
-    const stagiaire: Stagiaire = new Stagiaire()
-    stagiaire.setLastName(this.stagiaireForm.value.lastname)
-    stagiaire.setFirstName(this.stagiaireForm.value.firstname)
-    stagiaire.setEmail(this.stagiaireForm.value.email)
-    stagiaire.setPhoneNumber(this.stagiaireForm.value.phoneNumber)
-    if (this.stagiaireForm.value.birthDate != null) {
-      stagiaire.setBirthDate(new Date(this.stagiaireForm.value.birthDate))
-    }
-    console.log("Delegate add stagiaire:", stagiaire)
-    this.stagiaireService.add(stagiaire)
+    console.log("Delegate add stagiaire:", this.stagiaireForm.value)
+    const dto: StagiaireDto = new StagiaireDto(this.stagiaireForm.value)
+    this.stagiaireService.add(dto)
   }
 
 }
