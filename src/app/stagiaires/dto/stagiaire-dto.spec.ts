eef00cd99a52e8c0e0e5326a39cc99ee342bdd8b
DaCodeManiaK <jla.webprojet@gmail.com>
2022-11-28 15:10:22 +0100

    Separate DTO to transfer correct objet to API

---
+++ src/app/stagiaires/dto/stagiaire-dto.spec.ts
@@ -0,0 +1,7 @@
+import { StagiaireDto } from './stagiaire-dto';
+
+describe('StagiaireDto', () => {
+  it('should create an instance', () => {
+    expect(new StagiaireDto()).toBeTruthy();
+  });
+});
---
+++ src/app/stagiaires/dto/stagiaire-dto.ts
@@ -0,0 +1,20 @@
+export class StagiaireDto {
+    public lastname: string = '';
+    public firstname: string = '';
+    public email: string = '';
+    public phoneNumber: string = '';
+    public birthdate!: Date;
+
+    public constructor(formValues: any) {
+        Object.assign(this, formValues);
+        /**
+         * formValues : {
+         *  lastname: 'Aubert',
+         *  firstname: 'Jean-Luc',
+         *  email: 'jla.webprojet@gmail.com',
+         *  phoneNumber: '06 55 22 33 66',
+         *  birthDate: null
+         * }
+         */
+    }
+}
