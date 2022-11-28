495301f07b7a09dac0d667d6cb25ce83464b063c
DaCodeManiaK <jla.webprojet@gmail.com>
2022-11-28 13:34:56 +0100

    Exports some Components from Module

---
+++ src/app/material-ui/material-ui.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Forms UI
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ... MaterialUiModule.materialModules // ... SPREAD OPERATOR 
+  ]
+})
+export class MaterialUiModule {
+  public static materialModules = [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
+  ]
+}
