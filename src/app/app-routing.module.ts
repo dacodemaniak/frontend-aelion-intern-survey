9477f541c90b016a1e11c674ea51b21f0204f9f2
Jean-Luc <jean-luc.a@ideafactory.fr>
2022-11-29 12:17:05 +0100

    Add routes to stagiaire components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { StagiaireFormComponent } from './stagiaires/components/stagiaire-form/stagiaire-form.component';
+import { StagiaireTableComponent } from './stagiaires/components/stagiaire-table/stagiaire-table.component';
+import { StagiaireDetailComponent } from './stagiaires/stagiaire-detail/stagiaire-detail.component';
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(AppRoutingModule.routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  public static routes: Routes = [
+    {
+      path: '',
+      redirectTo: 'home', // Redirige vers un autre chemin ici 'home'
+      pathMatch: 'full'
+      // home/1
+      // home
+    },
+    {
+      path: 'home',
+      component: StagiaireTableComponent
+    },
+    {
+      path:'stagiaire/add',
+      component: StagiaireFormComponent
+    },
+    {
+      path: 'stagiaire/:id',
+      component: StagiaireDetailComponent
+    },
+    {
+      path: '**', // Wild card
+      redirectTo: 'home',
+      pathMatch: 'full'
+    }
+  ];
+}
