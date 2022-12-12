1f67fd34391c81fb0fbda4c6b9a7e21c47a97ebe
DaCodeManiaK <jla.webprojet@gmail.com>
2022-12-12 17:12:25 +0100

    User model implementation

--- src/app/user/models/user.ts
+++ src/app/user/models/user.ts
@@ -1,2 +1,32 @@
 export class User {
+    private _id?: number;
+    private _login: string = '';
+
+
+    /**
+     * User user = new User()
+     * user.id = 1
+     * console.log(user.id) // Expect 1
+     */
+    public get id(): number {
+        return this._id!;
+    }
+
+    /**
+     * User user = new User(): void
+     * user.id = 1
+     */
+    public set id(id: number) {
+        this._id = id;
+    }
+
+    public get login(): string {
+        return this._login;
+    }
+
+    public set login(login: string) {
+        this._login = login;
+    }
+
+
 }
