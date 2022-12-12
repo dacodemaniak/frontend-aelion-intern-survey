991fffe6f05f580cd193a198762798fd80b57b93
DaCodeManiaK <jla.webprojet@gmail.com>
2022-12-12 18:29:44 +0100

    Add poe services find methods

--- src/app/poe/models/poe.ts
+++ src/app/poe/models/poe.ts
@@ -1,2 +1,59 @@
+enum POE_TYPE {
+    POEI = 'POEI',
+    POEC = 'POEC'
+}
 export class Poe {
+    private _id?: number;
+    private _title: string = '';
+    private _beginDate!: Date;
+    private _endDate!: Date;
+    private _type: POE_TYPE = POE_TYPE.POEI;
+
+    get id() {
+      return this._id!
+    }
+    
+    set id(val: number) {
+      this._id = val
+    }
+    
+    get title() {
+      return this._title
+    }
+    
+    set title(val: string) {
+      this._title = val
+    }
+    
+    get beginDate() {
+      return this._beginDate
+    }
+    
+    set beginDate(val: Date) {
+      this._beginDate = val
+    }
+    
+    get endDate() {
+      return this._endDate
+    }
+    
+    set endDate(val: Date) {
+      this._endDate = val
+    }
+    
+    get type() {
+      return this._type
+    }
+    
+    set type(val: POE_TYPE) {
+      this._type = val
+    }
+
+    public deserialize(datas: any): void {
+        this._id = datas.id;
+        this._title = datas.title;
+        this._beginDate = new Date(datas.beginDate);
+        this._endDate = new Date(datas.endDate);
+        this._type = datas.type    
+    }
 }
--- src/app/poe/services/poe.service.ts
+++ src/app/poe/services/poe.service.ts
@@ -1,9 +1,45 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { take, map } from 'rxjs/operators';
+import { Poe } from '../models/poe';
+import { environment } from './../../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class PoeService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  public findAll(): Observable<Poe[]> {
+    return this.httpClient.get<any>(
+      `${environment.apiBaseUrl}poe`
+    )
+    .pipe(
+      take(1),
+      map((persistentPoes: any[]) => {
+        return persistentPoes.map((persistentPoe) => {
+          const poe: Poe = new Poe();
+          poe.deserialize(persistentPoe);
+          return poe;
+        })
+      })
+    )
+  }
+
+  public findOne(id: number): Observable<Poe> {
+    return this.httpClient.get<any>(
+      `${environment.apiBaseUrl}poe`
+    )
+    .pipe(
+      take(1),
+      map((persistentPoe: any) => {
+        const poe: Poe = new Poe();
+        poe.deserialize(persistentPoe);
+        return poe;
+      })
+    )    
+  }
 }
