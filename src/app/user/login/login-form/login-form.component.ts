6ab248ee3b4a01387001dfe3a07d864955e4c3e9
DaCodeManiaK <jla.webprojet@gmail.com>
2022-12-12 17:09:46 +0100

    Login controller implementation

--- src/app/user/login/login-form/login-form.component.ts
+++ src/app/user/login/login-form/login-form.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subscription, take } from 'rxjs';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss']
 })
-export class LoginFormComponent implements OnInit {
+export class LoginFormComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public loginForm!: FormGroup;
+  private _subscription!: Subscription;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      userLogin: [
+        '',
+        [
+          Validators.required
+        ]
+      ],
+      userPassword: [
+        '',
+        [
+          Validators.required
+        ]
+      ],
+      stayConnected: [
+        false
+      ]
+    });
   }
 
+  ngOnDestroy(): void {
+      if (this._subscription !== undefined) {
+        this._subscription.unsubscribe();
+      }
+  }
+
+  public onLogin(): void {
+    this._subscription = this.userService.login(this.loginForm.value)
+      .subscribe((authenticated: boolean) => {
+        if (authenticated) {
+          this.router.navigate(['/', 'home']);
+        } else {
+          this.loginForm.reset();
+        }
+      })
+  }
 }
