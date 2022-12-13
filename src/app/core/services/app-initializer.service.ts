052051357d0ec626c8b0c87b1b0345c514aeba29
Jean-Luc <jean-luc.a@ideafactory.fr>
2022-12-13 11:41:14 +0100

    Implements APP_INITIALIZER token

---
+++ src/app/core/services/app-initializer.service.ts
@@ -0,0 +1,38 @@
+import { APP_INITIALIZER, Injectable } from '@angular/core';
+import { UserService } from 'src/app/user/services/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppInitializerService {
+
+  constructor(
+    private _userService: UserService
+  ) { }
+
+  public init(): Promise<void> {
+    return new Promise<any>((resolve: any) => {
+      const hasUser$ = this._userService.hasUser();
+      if (hasUser$.getValue()) {
+        console.log(`A user was stored, so let's go`);
+      }
+      resolve();
+    })
+  }
+}
+
+
+export const initializeApp = (appInitService: AppInitializerService): any => {
+  return (): Promise<void> => {
+    return appInitService.init();
+  }
+}
+
+export const appInit = {
+  provide: APP_INITIALIZER,
+  useFactory: initializeApp, // Utilise la fonction qui instancie le service et appelle la méthode init()
+  deps: [ // DI à la main
+    AppInitializerService
+  ],
+  multi: true
+}
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginFormComponent } from './login/login-form/login-form.component';
 import { SharedModule } from '../shared/shared.module';
+import { appInit } from '../core/services/app-initializer.service';
 
 
 
@@ -12,6 +13,9 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     CommonModule,
     SharedModule
+  ],
+  providers: [
+    appInit
   ]
 })
 export class UserModule { }
