be3ee5f901e51e4d25384e35853a63c7748dd49c
Jean-Luc <jean-luc.a@ideafactory.fr>
2022-12-13 08:48:43 +0100

    HasUser implementation

--- src/app/user/guards/no-user.guard.ts
+++ src/app/user/guards/no-user.guard.ts
@@ -6,10 +6,20 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class NoUserGuard implements CanActivate {
+  public constructor(
+    private userService: UserService,
+    private router: Router
+  ) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.userService.hasUser().getValue) {
+      this.router.navigate(['/', 'home']);
+      return false;
+    }
+
     return true;
   }
-  
+
 }
